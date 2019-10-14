c2c7a442316e156b45ca2b43cd405a6fc36db1b9
HP <HP@LAPTOP-HBOCIAMD>
2019-10-15 00:35:55 +0530

    first commit

---
+++ Maven/src/test/java/P1/SampleTest.java
@@ -0,0 +1,26 @@
+package P1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test
+	public void m1()
+	{
+	System.out.println("From m1");
+	}
+@Test
+	public void m2()
+	{
+	System.out.println("From m2");
+	}
+@Test
+	public void m3()
+	{
+	System.out.println("From m3");
+	}
+@Test
+public void m4()
+	{
+	System.out.println("From m4");
+	}
+}
